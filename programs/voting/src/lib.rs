297d25804bf123c791e3f2b11f6bcefe31ef3a93
jatin-j <infinitywastaken80808080@gmail.com>
2026-04-09 17:36:26 +0530

    feat: add poll instruction

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -34,6 +34,18 @@ pub mod voting {
 
         Ok(())
     }
+    pub fn vote(ctx: Context<Vote>, _poll_id: u32, _candidate: String) -> Result<()> {
+        let current_time = Clock::get()?.unix_timestamp;
+
+        if current_time > ctx.accounts.poll_account.poll_end_time as i64 {
+            return err!(VoteError::VotingFinished);
+        } else if current_time < ctx.accounts.poll_account.poll_start_time as i64 {
+            return err!(VoteError::VoteNotStarted);
+        }
+
+        ctx.accounts.candidate_account.canditate_votes += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -74,6 +86,27 @@ pub struct InitCandidate<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(poll_id:u32, candidate:String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut,
+        seeds=[b"poll",signer.key().as_ref(),poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+    #[account(
+        init,
+        payer=signer,
+        space=8+CandidateAccount::INIT_SPACE,
+        seeds=[poll_id.to_le_bytes().as_ref(),candidate.as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct PollAccount {
@@ -92,4 +125,12 @@ pub struct CandidateAccount {
     #[max_len(32)]
     pub candidate_name: String,
     pub canditate_votes: u64,
+}
+
+#[error_code]
+pub enum VoteError {
+    #[msg("Voting has not started yet!")]
+    VoteNotStarted,
+    #[msg("Time up! Voting is finished!")]
+    VotingFinished,
 }
